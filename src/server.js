49b4bc4744895c9b9178b875b504602d675795c6
Cheaeyon Jeon <chaenii.jeon@gmail.com>
2022-07-06 23:35:05 +0900

    fix: change index.js file name t0 server.js

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+const handleHome = (req, res) => {
+  return res.send("I still love you 💖");
+};
+const handleLogin = (req, res) => {
+  return res.send({ message: "Login Here ~ 👌" });
+};
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
+const handleListening = () => console.log(`🐱‍🐉 Server listenting on port http://localhost:${PORT} 😂`);
+
+app.listen(PORT, handleListening);
