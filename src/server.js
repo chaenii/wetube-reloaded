cd2bf66e18e8023bca09e4f2bba709ed231adc75
Cheaeyon Jeon <chaenii.jeon@gmail.com>
2022-07-09 23:51:30 +0900

    add Routers, Controllers and update server.js

--- src/server.js
+++ src/server.js
@@ -1,18 +1,18 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
+app.use(logger);
 
-const handleHome = (req, res) => {
-  return res.send("I still love you 💖");
-};
-const handleLogin = (req, res) => {
-  return res.send({ message: "Login Here ~ 👌" });
-};
-
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 const handleListening = () => console.log(`🐱‍🐉 Server listenting on port http://localhost:${PORT} 😂`);
 
