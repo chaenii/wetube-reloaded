6d1436a10d371300c1fad0c9f031bde2a8d2c37c
Cheaeyon Jeon <chaenii.jeon@gmail.com>
2022-07-11 23:09:14 +0900

    feat: using pug template -rendering page and include base page

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,6 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => {
-  return res.send(`Watch video #${req.params.id}`);
-};
-export const edit = (req, res) => {
-  return res.send("Edit Videos");
-};
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Search Videos");
 export const upload = (req, res) => res.send("Upload Videos");
 export const deleteVideo = (req, res) => {
