007ac4c16bb508b37a8fc82315a52d5ef7e3f39e
Cheaeyon Jeon <chaenii.jeon@gmail.com>
2022-07-12 23:07:43 +0900

    feat:mixins video block/ fix:using iterator

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,37 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+// const fakeUser = {
+//   username: "jobchae",
+//   loggedIn: false,
+// };
+
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes age",
+      views: 50,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes age",
+      views: 50,
+      id: 1,
+    },
+    {
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes age",
+      views: 50,
+      id: 1,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
+};
 export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Search Videos");
